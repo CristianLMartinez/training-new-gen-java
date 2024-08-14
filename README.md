600f7f86ec385fe689d1f45b47bde58625e9aedc
CristianLMartinez <cristian.lm@globant.com>
2024-08-14 16:54:24 -0500

    starter project: add project dependencies, configure h2 database, add git workflow and application configurations

---
+++ README.md
@@ -0,0 +1,11 @@
+# Javastart :star:
+
+Training new geneneration for java....
+
+
+### Clone this repository 
+
+```shell
+git clone https://github.com/CristianLMartinez/training-new-gen-java.git
+cd training-new-gen-java
+```
---
+++ src/main/java/com/datastructures/trainingnewgen/TrainingNewGenApplication.java
@@ -0,0 +1,13 @@
+package com.datastructures.trainingnewgen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainingNewGenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrainingNewGenApplication.class, args);
+    }
+
+}
