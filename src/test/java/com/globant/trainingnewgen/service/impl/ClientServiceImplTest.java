6af6e69fe60cc094d42cf32624f299876a420dbc
David Emanuel Barragan Munoz <david.barragan@globant.com>
2024-09-11 16:53:28 -0500

    falta el delete y arreglar el update del clientTest

--- src/test/java/com/globant/trainingnewgen/service/impl/ClientServiceImplTest.java
+++ src/test/java/com/globant/trainingnewgen/service/impl/ClientServiceImplTest.java
@@ -1,7 +1,157 @@
 package com.globant.trainingnewgen.service.impl;
 
-import static org.junit.jupiter.api.Assertions.*;
+import com.globant.trainingnewgen.exception.custom.EntityConflictException;
+import com.globant.trainingnewgen.exception.custom.ExceptionCode;
+import com.globant.trainingnewgen.exception.custom.ResourceNotFoundException;
+import com.globant.trainingnewgen.model.dto.ClientDto;
+import com.globant.trainingnewgen.model.entity.Client;
+import com.globant.trainingnewgen.repository.ClientRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.Optional;
+
+import static org.hamcrest.Matchers.any;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.anyBoolean;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+
+@ExtendWith(MockitoExtension.class)
+@DisplayName("Test client service implementation")
 class ClientServiceImplTest {
+
+    @Mock
+    private ClientRepository clientRepository;
+
+    @InjectMocks
+    private ClientServiceImpl clientService;
+
+    @Test
+    void testCreateClient() {
+        // GIVEN
+        var clientDto = ClientDto.builder()
+                .document("CC-1234569")
+                .build();
+
+        Client client = Client.builder()
+                .document(clientDto.document())
+                .isDeleted(false)
+                .build();
+
+        // WHEN
+        when(clientRepository.findClientByDocument(anyString())).thenReturn(Optional.empty());
+        when(clientRepository.save(client)).thenReturn(client);
+
+        ClientDto clientReturned = clientService.create(clientDto);
+
+        // THEN
+        verify(clientRepository).save(client);
+        assertEquals( clientDto.document(), clientReturned.document());
+
+    }
+
+    @Test
+    @DisplayName("Test create client when a client already exists should restore it")
+    void createWhenClientExistsAndIsDeletedShouldRestoreClient() {
+        var clientDto = ClientDto.builder()
+                .document("CC-1234567890")
+                .build();
+
+        Client existingClient = new Client();
+        existingClient.setDeleted(true);
+
+        when(clientRepository.findClientByDocument(anyString())).thenReturn(Optional.of(existingClient));
+        when(clientRepository.save(existingClient)).thenReturn(existingClient);
+
+
+        ClientDto restoredClient = clientService.create(clientDto);
+
+        verify(clientRepository, times(1)).findClientByDocument(eq("CC-1234567890"));
+        assertNotNull(restoredClient);
+        assertFalse(existingClient.isDeleted(), "Field *isDeleted* should be false");
+    }
+
+    @Test
+    @DisplayName("Test search client when this exist ")
+    void testSearchClient() {
+
+        String document = "CC-1234569";
+        Boolean isDeleted = false;
+
+        Client mockClient = new Client();
+
+        when(clientRepository.findClientByDocument(anyString(),anyBoolean())).thenReturn(Optional.of(mockClient));
+
+        ClientDto clientDto = clientService.getClientByDocument(document, isDeleted);
+
+        assertNotNull(clientDto);
+        verify(clientRepository, times(1)).findClientByDocument(document, isDeleted);
+    }
+
+    @Test
+    @DisplayName("Test search client when it exists")
+    void testSearchClientShouldThrowException() {
+        String document = "CC-1234569";
+        Boolean isDeleted = false;
+
+        var exception = new ResourceNotFoundException(
+                String.format("Can't find user with document %s", document),
+                ExceptionCode.CLIENT_NOT_FOUND);
+
+        when(clientRepository.findClientByDocument(anyString(), anyBoolean())).thenThrow(exception);
+
+        assertThrows(ResourceNotFoundException.class, () -> clientService.getClientByDocument(document, isDeleted));
+    }
+
+    @Test
+    void testUpdateClient_Success() {
+        // Arrange
+        String document = "CC-123456";
+        ClientDto requestBody = new ClientDto.ClientDtoBuilder()
+                .name("John Doe")
+                .email("john.doe@example.com")
+                .phone("123456789")
+                .deliveryAddress("123 Main St")
+                .build();
+
+
+        Client existingClient = new Client(); // Simula un cliente existente
+        existingClient.setName("Old Name"); // Cambiamos los valores para que haya diferencia con el DTO
+
+        // Simulamos la recuperación y la verificación de cambios
+        when(clientService.validateAndRetrieveClientByDocument(document, false)).thenReturn(existingClient);
+        when(clientService.hasChanges(existingClient, requestBody)).thenReturn(false); // Significa que hay cambios
+
+        // Act
+        clientServiceImpl.updateClient(document, requestBody);
+
+
+        // Verificamos que los setters se llamaron correctamente y el cliente fue actualizado
+        assertEquals(requestBody.name(), existingClient.getName());
+        assertEquals(requestBody.email(), existingClient.getEmail());
+        assertEquals(requestBody.phone(), existingClient.getPhone());
+        assertEquals(requestBody.deliveryAddress(), existingClient.getDeliveryAddress());
+
+        // Verificamos que se llamó al método save del repositorio
+        verify(clientRepository, times(1)).save(existingClient);
+    }
+
+
+
+
+
 
 }
